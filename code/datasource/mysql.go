78fe64043afb6e188255f3b46a559a0d4a94cd33
Zura <zura@lider-bet.com>
2021-11-05 17:47:39 +0400

    log with logrus

--- code/datasource/mysql.go
+++ code/datasource/mysql.go
@@ -2,7 +2,6 @@ package datasource
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 	"os"
@@ -15,23 +14,19 @@ var (
 func init() {
 	_, found := os.LookupEnv("db_user")
 	if !found {
-		fmt.Println("environment Variable 'db_user' not found")
-		os.Exit(1)
+		log.Fatal("environment Variable 'db_user' not found")
 	}
 	_, found = os.LookupEnv("db_pass")
 	if !found {
-		fmt.Println("environment Variable 'db_pass' not found")
-		os.Exit(1)
+		log.Fatal("environment Variable 'db_pass' not found")
 	}
 	_, found = os.LookupEnv("db_host")
 	if !found {
-		fmt.Println("environment Variable 'db_host' not found")
-		os.Exit(1)
+		log.Fatal("environment Variable 'db_host' not found")
 	}
 	_, found = os.LookupEnv("db_port")
 	if !found {
-		fmt.Println("environment Variable 'db_port' not found")
-		os.Exit(1)
+		log.Fatal("environment Variable 'db_port' not found")
 	}
 	var err error
 	dbURL := os.ExpandEnv("${db_user}:${db_pass}@tcp(${db_host}:${db_port})/${db_name}?autocommit=true")
